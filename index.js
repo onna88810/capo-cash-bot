0d4d4bc61a323c7e612eb417bc9fa24a5ca26658
onna88810 <onnawelch@gmail.com>
2026-02-14 23:34:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const DISCORD_APP_ID = process.env.DISCORD_APP_ID;
 const COMMAND_GUILD_ID = process.env.COMMAND_GUILD_ID;
 
 const COIN = "<a:CC:1472374417920229398>";
+const fmtNum = (n) => Number(n ?? 0).toLocaleString("en-US");
 
 if (!DISCORD_TOKEN || !DISCORD_APP_ID) {
   throw new Error("Missing DISCORD_TOKEN or DISCORD_APP_ID env vars.");
